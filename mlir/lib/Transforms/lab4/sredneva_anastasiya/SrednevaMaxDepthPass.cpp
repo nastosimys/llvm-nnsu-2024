6e748fb6b3dd16ed6298a04cedd9b75a19e1c7ba
Nastya <nastosimys2000@gmail.com>
2024-05-20 01:03:01 +0300

    commit_5

--- mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
+++ mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
@@ -40,55 +40,6 @@ public:
                           IntegerType::get(funcOp.getContext(), 32), maxDepth));
     });
   }
-
-  /* void runOnOperation() override {
-    FuncOp func = getOperation();
-    int maxDepth = getMaxDepth(func.getBody());
-    func->setAttr(
-        "maxDepth",
-        IntegerAttr::get(IntegerType::get(getContext(), 32), maxDepth));
-  }
-
-private:
-  int getMaxDepth(Block &block) {
-    int maxDepth = 1;
-    for (Operation &op : block) {
-      if (auto regionOp = dyn_cast<RegionBranchOpInterface>(&op)) {
-        for (Region &region : regionOp.getRegions()) {
-          int depth = 1 + getMaxDepth(region.front());
-          if (depth > maxDepth) {
-            maxDepth = depth;
-          }
-        }
-      }
-    }
-    return maxDepth;
-    /* int maxDepth = 1;
-    ModuleOp module = getOperation();
-
-    module.walk([&](Operation *op) {
-      if (auto func = dyn_cast<LLVM::LLVMFuncOp>(op)) {
-        int currentDepth = 1;
-        while (op) {
-          if (auto *regionOp = dyn_cast<RegionBranchOpInterface>(op)) {
-            currentDepth++;
-            op = regionOp.getRegion().front().getTerminator();
-          } else {
-            break;
-          }
-        }
-        maxDepth = std::max(maxDepth, currentDepth);
-      }
-    });
-    module.setAttr(
-        "MaxDepth",
-        IntegerAttr::get(IntegerType::get(getContext(), 32), maxDepth));
-    /* func->setAttr(
-        "MaxDepth",
-        IntegerAttr::get(IntegerType::get(func.getContext(), 32),
-                         maxDepth));
-  });*/
-}
 };
 } // namespace
 
