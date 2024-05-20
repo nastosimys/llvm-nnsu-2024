532454f6f6fc6a7f8303083fd882057f665ca51c
Nastya <nastosimys2000@gmail.com>
2024-05-20 19:26:52 +0300

    39

--- mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
+++ mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
@@ -31,6 +31,9 @@ private:
     int maxDepth = 1;
     std::function<void(Operation *, int)> calculateDepth = [&](Operation *op,
                                                                int depth) {
+      if (op != nullptr) {
+        maxDepth = std::max(maxDepth, depth);
+      }
       if (op->getNumRegions() > 0) {
         Region &region = op->getRegion(0);
         if (!region.empty()) {
