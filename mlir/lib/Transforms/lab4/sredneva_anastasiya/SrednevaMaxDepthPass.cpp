b74158c20d829f27c63f3a7ea34d884079f53c2d
Nastya <nastosimys2000@gmail.com>
2024-05-21 20:44:51 +0300

    70

--- mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
+++ mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
@@ -8,13 +8,12 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h" //
 #include "mlir/IR/BuiltinOps.h"           //
 #include "mlir/IR/PatternMatch.h"         //
-#include <stack>
 
 using namespace mlir;
 
 namespace {
 class SrednevaMaxDepthPass
-    : public PassWrapper<SrednevaMaxDepthPass, OperationPass<func::FuncOp>> {
+    : public PassWrapper<SrednevaMaxDepthPass, OperationPass<ModuleOp>> {
 public:
   StringRef getArgument() const final { return "SrednevaMaxDepthPass"; }
   StringRef getDescription() const final {
@@ -22,27 +21,30 @@ public:
   }
 
   void runOnOperation() override {
-    SmallVector<std::pair<Operation *, int>, 8> stack;
-    getOperation()->walk([&](Operation *op) { stack.emplace_back(op, 0); });
 
-    while (!stack.empty()) {
-      auto [op, currentDepth] = stack.back();
-      stack.pop_back();
-
-      int maxDepth = currentDepth;
-
-      for (Region &region : op->getRegions()) {
-        for (Block &block : region) {
-          for (Operation &nestedOp : block) {
-            stack.emplace_back(&nestedOp, currentDepth + 1);
-          }
-        }
-      }
+    getOperation()->walk([&](Operation *op) {
+      int maxDepth = computeMaxDepth(op);
 
       op->setAttr(
           "maxDepth",
           IntegerAttr::get(IntegerType::get(op->getContext(), 32), maxDepth));
+    });
+  }
+
+private:
+  int computeMaxDepth(Operation *op, int currentDepth = 0) {
+    int maxDepth = currentDepth;
+
+    for (Region &region : op->getRegions()) {
+      for (Block &block : region) {
+        for (Operation &nestedOp : block) {
+          maxDepth =
+              std::max(maxDepth, computeMaxDepth(&nestedOp, currentDepth + 1));
+        }
+      }
     }
+
+    return maxDepth;
   }
 };
 } // namespace
