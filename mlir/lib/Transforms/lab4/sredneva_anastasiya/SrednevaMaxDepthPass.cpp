b4b41f48c00c20a81bb637cdb117f1b8a269df02
Nastya <nastosimys2000@gmail.com>
2024-05-23 00:04:49 +0300

    18

--- mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
+++ mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
@@ -13,42 +13,34 @@ using namespace mlir;
 
 namespace {
 class SrednevaMaxDepthPass
-    : public PassWrapper<SrednevaMaxDepthPass, OperationPass<func::FuncOp>> {
+    : public PassWrapper<SrednevaMaxDepthPass, OperationPass<ModuleOp>> {
 public:
   StringRef getArgument() const final { return "SrednevaMaxDepthPass"; }
   StringRef getDescription() const final {
     return "Counts the max depth of region nests in the function.";
   }
   void runOnOperation() override {
-    getOperation().walk([&](Operation *op) {
-      if (auto funcOp = dyn_cast<LLVM::LLVMFuncOp>(op)) {
-        int maxDepth = getMaxDepth(&funcOp);
-        funcOp->setAttr(
-            "maxDepth",
-            IntegerAttr::get(IntegerType::get(funcOp.getContext(), 32),
-                             maxDepth));
-      }
+    getOperation()->walk([&](Operation *op) {
+      int maxDepth = 0;
+
+      std::function<void(Operation *, int)> getMaxDepth =
+          [&](Operation *op, int depth) {
+            maxDepth = std::max(maxDepth, depth);
+            for (Region &region : op->getRegions()) {
+              for (Block &block : region) {
+                for (Operation &op2 : block) {
+                  getMaxDepth(&op2, depth + 1);
+                }
+              }
+            }
+          };
+
+      getMaxDepth(op, 0);
+
+      op->setAttr(
+          "maxDepth",
+          IntegerAttr::get(IntegerType::get(op->getContext(), 32), maxDepth));
     });
-  }
-
-private:
-  int getMaxDepth(LLVM::LLVMFuncOp *funcOp) {
-    int maxDepth = 1;
-    std::function<void(Operation *, int)> calculateDepth = [&](Operation *op,
-                                                               int depth) {
-      if (op->getNumRegions() > 0) {
-        Region &region = op->getRegion(0);
-        for (Operation &nestedOp : region.front()) {
-          maxDepth = std::max(maxDepth, depth + 1);
-          calculateDepth(&nestedOp, depth + 1);
-        }
-      }
-    };
-
-    for (Operation &op : funcOp->getOps()) {
-      calculateDepth(&op, 1);
-    }
-    return maxDepth;
   }
 };
 } // namespace
