bb3023f44c021b8b2d1b383ef32043e8592fbf8d
Nastya <nastosimys2000@gmail.com>
2024-05-22 19:41:59 +0300

    13

--- mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
+++ mlir/lib/Transforms/lab4/sredneva_anastasiya/SrednevaMaxDepthPass.cpp
@@ -8,7 +8,6 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h" //
 #include "mlir/IR/BuiltinOps.h"           //
 #include "mlir/IR/PatternMatch.h"         //
-#include <stack>
 
 using namespace mlir;
 
@@ -23,7 +22,7 @@ public:
   void runOnOperation() override {
     getOperation().walk([&](Operation *op) {
       if (auto funcOp = dyn_cast<LLVM::LLVMFuncOp>(op)) {
-        int maxDepth = getMaxDepth(funcOp.getBody());
+        int maxDepth = getMaxDepth(&funcOp);
         funcOp->setAttr(
             "sredneva.maxDepth",
             IntegerAttr::get(IntegerType::get(funcOp.getContext(), 32),
@@ -33,31 +32,22 @@ public:
   }
 
 private:
-  int getMaxDepth(Region &region) {
-    std::stack<std::pair<Region *, int>> stack;
-    stack.push({&region, 1});
-    int maxDepth = 0;
-
-    while (!stack.empty()) {
-      auto [currentRegion, currentDepth] = stack.top();
-      stack.pop();
-      maxDepth = std::max(maxDepth, currentDepth);
-
-      for (Block &block : currentRegion->getBlocks()) {
-        for (Operation &op : block) {
-          int nestedDepth = currentDepth;
-          if (op.hasTrait<OpTrait::IsTerminator>()) {
-            nestedDepth++;
-          }
-          for (Region &nestedRegion : op.getRegions()) {
-            if (!nestedRegion.empty()) {
-              stack.push({&nestedRegion, nestedDepth});
-            }
-          }
+  int getMaxDepth(LLVM::LLVMFuncOp *funcOp) {
+    int maxDepth = 1;
+    std::function<void(Operation *, int)> calculateDepth = [&](Operation *op,
+                                                               int depth) {
+      if (op->getNumRegions() > 0) {
+        Region &region = op->getRegion(0);
+        for (Operation &nestedOp : region.front()) {
+          maxDepth = std::max(maxDepth, depth + 1);
+          calculateDepth(&nestedOp, depth + 1);
         }
       }
-    }
+    };
 
+    for (Operation &op : funcOp->getOps()) {
+      calculateDepth(&op, 1);
+    }
     return maxDepth;
   }
 };
